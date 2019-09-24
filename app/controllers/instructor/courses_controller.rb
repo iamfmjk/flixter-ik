e14cacf10d9909838f6c708e18a5258ca4669148
Iuliia <julika27@gmail.com>
2019-09-24 09:12:38 +0000

    Permissions for Course and Section

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,6 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def new
     @course = Course.new
@@ -15,10 +16,20 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def show
-    @course = Course.find(params[:id])
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+ def current_course
+   @current_course ||= Course.find(params[:id])
+ end
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
