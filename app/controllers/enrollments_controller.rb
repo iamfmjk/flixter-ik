52e5d908e8bdb495d2a1f0c805ce7a7010d31d8e
Iuliia <julika27@gmail.com>
2019-10-09 18:09:36 +0000

    Integrated Stripe payments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,8 +2,29 @@ class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    if current_course.premium?
+      # Amount in cents
+      @amount = (current_course.cost * 100).to_i
+
+      customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+      )
+
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Flixter Premium content',
+        currency: 'usd'
+      )
+    end
+
     current_user.enrollments.create(course: current_course)
     redirect_to course_path(current_course)
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to course_path(current_course)
   end
 
   private
--- app/models/course.rb
+++ app/models/course.rb
@@ -2,10 +2,18 @@ class Course < ApplicationRecord
   belongs_to :user
   has_many :sections
   has_many :enrollments
-  
+
   mount_uploader :image, ImageUploader
 
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
+
+  def free?
+    cost.zero?
+  end
+
+  def premium
+    ! free?
+  end
 end
