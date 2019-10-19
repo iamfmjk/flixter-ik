2323c0f8e7ef28d53bdb678050db1a6929b7da97
Iuliia <julika27@gmail.com>
2019-10-19 21:34:31 +0000

    Implemented reordering for lessons, prepared reordering for sections

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,6 +1,10 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section
+  before_action :require_authorized_for_current_section, only: [:new, :create]
+  before_action :require_authorized_for_current_lesson, only: [:update]
+
+
+
   def new
     @lesson = Lesson.new
   end
@@ -10,11 +14,26 @@ class Instructor::LessonsController < ApplicationController
     redirect_to instructor_course_path(current_section.course)
   end
 
+  def update
+    current_lesson.update_attributes(lesson_params)
+    render plain: 'updated!'
+  end
+
   private
+
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
       return render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
@@ -24,6 +43,6 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video)
+    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_13_130433) do
+ActiveRecord::Schema.define(version: 2019_10_19_203926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,9 @@ ActiveRecord::Schema.define(version: 2019_10_13_130433) do
     t.integer "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "row_order"
     t.index ["course_id"], name: "index_sections_on_course_id"
+    t.index ["row_order"], name: "index_sections_on_row_order"
   end
 
   create_table "users", force: :cascade do |t|
