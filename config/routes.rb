3a1d803a8f7278e95a44925b8af8dc048f90df88
Iuliia <julika27@gmail.com>
2019-10-24 07:37:58 +0000

    Added user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resource :dashboard, only: [:show]
+
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
